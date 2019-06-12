f90582519a2bfe951d4e92bf6f77bde1a37582ef
sabahat278 <maliksabahat786@gmail.com>
2019-06-13 01:33:48 +0500

    initial commit 1

---
+++ src/labTerminalB/Driver.java
@@ -0,0 +1,12 @@
+package labTerminalB;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		String input = "The <b>Good</b>, the <i>Bad</i>, and the <strong>Ugly</strong>";
+		String regex = "TODO";
+		String output = input.replaceAll(regex, "");
+
+	}
+
+}
