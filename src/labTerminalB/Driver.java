13cbb23678415ad574aa86503aefb0a359e3dd67
munazza basharat <munazzabibi916@gmail.com>
2019-06-13 10:23:58 +0500

    regular expression created

--- src/labTerminalB/Driver.java
+++ src/labTerminalB/Driver.java
@@ -10,25 +10,30 @@ public class Driver {
 	 * 		"TODO 'TASK NUMBER' Complete" e.g. in this case "TODO 1 COMPLETED"
 	 */
 	public static void main(String[] args) {
-		String input = "The <b>Good</b>, the <i>Bad</i>, and the <strong>Ugly</strong>";
+		String input = "The <b>Good</b>,the<i>bad</i>,and the<strong>Ugly</strong>"; ;
 		String regex = "TODO 1";
-		String output = input.replaceAll(regex, "");
+		String output = input.replaceAll("\\<.*?>","") ;
 		System.out.println(output);
 		
+	
 		
-		Movie m1 = new Movie("Spancer", 0);
-		Movie m2 = new Movie("Speed", 1);
-		Movie m3 = new Movie("Blood Diamond",2);
-		Customer me = new Customer("Nadir");
-		Customer me2 = new Customer("Allah Ditta");
-		Rental r1 = new Rental(m1, 23);
-		Rental r2 = new Rental(m2,4);
-		Rental r3 = new Rental(m3,3);
-		me.addRental(r1);
-		me.addRental(r2);
-		me2.addRental(r3);
-		System.out.println(me.statement());
-		System.out.println(me2.statement());
+		Movie movie1= new Movie("Spancer", 0);
+		Movie movie2= new Movie("Speed", 1);
+		Movie movie3= new Movie("Blood Diamond", 2);
+		
+		Customer customer1 = new Customer("Nadir");
+		Customer customer2 = new Customer("Allah Ditta");
+		
+		Rental r1=new Rental(movie2, 5);
+		Rental r2=new Rental(movie3, 6);
+		Rental r3=new Rental(movie1, 10);
+		
+		customer1.addRental(r1);
+		customer1.addRental(r3);
+		
+		System.out.println(customer1.statement());
 
 	}
+
+	
 }
